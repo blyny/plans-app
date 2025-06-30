096052e4dbf930316a218232709c62ec28e3ab64
bran-lang <bly0529p@gmail.com>
2025-06-30 13:36:32 -0400

    updates

--- myapp.py
+++ myapp.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 import json
+import requests
 
 app = Flask(__name__)  # referencing to the current file
 # database URL, other databases can be used
@@ -13,16 +14,17 @@ class Plan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     activity = db.Column(db.String(200), nullable=False)
     location = db.Column(db.String(300), nullable=False)
-    people = db.Column(db.Text) # store as a json string
+    people = db.Column(db.Text)  # store as a json string
     plan_date = db.Column(db.DateTime, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True),
                              default=lambda: datetime.now(timezone.utc))
-    
+
     def set_people(self, people_list):
         self.people = json.dumps(people_list)
+
     def get_people(self):
         return json.loads(self.people)
-    
+
     def __repr__(self):
         return f'<Plan {self.id}>'
 
@@ -34,25 +36,50 @@ def index():
         location_content = request.form['location']
         people_content = request.form['people']
         plan_date_str = request.form['plan_for']
-        
-        plan_date = datetime.fromisoformat(plan_date_str)
-        if plan_date <= datetime.now():
-            return "Unless it's today or you can go back in time.... pick a future date"
-        
-        people_list = [p.strip() for p in people_content.split(',') if p.strip()]
-        
-        new_activity = Plan(activity=activity_content, location = location_content, plan_date=plan_date)
+
+        if plan_date_str:
+            plan_date = datetime.fromisoformat(plan_date_str)
+            if plan_date <= datetime.now():
+                return "Unless it's today or you can go back in time.... pick a future date"
+        else:
+            return "PICK A DATE"
+
+        people_list = [p.strip()
+                       for p in people_content.split(',') if p.strip()]
+
+        new_activity = Plan(activity=activity_content,
+                            location=location_content, plan_date=plan_date)
         new_activity.set_people(people_list)
-        
+
         try:
             db.session.add(new_activity)
             db.session.commit()
+            # send_discord_notification()
             return redirect('/')
         except:
             "Err.. trouble adding the activity to the plan"
     else:
         activities = Plan.query.order_by(Plan.date_created).all()
-        return render_template('index.html', activities = activities)
+        return render_template('index.html', activities=activities)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    plan_to_delete = Plan.query.get_or_404(id)
+    try:
+        db.session.delete(plan_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "We cant delete it?"
+
+
+def send_discord_notification():
+    webhook_url = 'https://discord.com/api/webhooks/1389077022671900714/ecG_g5MsGOJCdNlu7wuXfcDALnlhNtW8hyiC3HOG1Gltwj2EOq7bILQIecSkk38LekAV'
+    data = {
+        "content": f"@everyone 📅 New Plan Created!"
+    }
+    requests.post(webhook_url, json=data)
 
 
 if __name__ == '__main__':
