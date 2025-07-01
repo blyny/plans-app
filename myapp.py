0c81a56472f10d4a71b35df6313a469e002a966a
bran-lang <bly0529p@gmail.com>
2025-06-30 23:55:02 -0400

    added create page

--- myapp.py
+++ myapp.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from dotenv import load_dotenv
+import os
 import json
 import requests
+load_dotenv()
 
+TOKEN = os.getenv("webhook")
 app = Flask(__name__)  # referencing to the current file
 # database URL, other databases can be used
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -30,32 +34,7 @@ class Plan(db.Model):
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        activity_content = request.form['activity']
-        location_content = request.form['location']
-        people_content = request.form['people']
-        plan_date_str = request.form['plan_for']
-
-        if plan_date_str:
-            plan_date = datetime.fromisoformat(plan_date_str)
-            if plan_date <= datetime.now():
-                return "Unless it's today or you can go back in time.... pick a future date"
-        else:
-            return "Did you fill everything out?"
-
-        people_list = [p.strip()
-                       for p in people_content.split(',') if p.strip()]
-
-        new_activity = Plan(activity=activity_content,
-                            location=location_content, plan_date=plan_date)
-        new_activity.set_people(people_list)
-
-        try:
-            db.session.add(new_activity)
-            db.session.commit()
-            # send_discord_notification()
-            return redirect('/')
-        except:
-            "Err.. trouble adding the activity to the plan"
+        pass
     else:
         activities = Plan.query.order_by(Plan.plan_date).all()
         return render_template('index.html', activities=activities)
@@ -86,7 +65,7 @@ def create():
         try:
             db.session.add(new_activity)
             db.session.commit()
-            # send_discord_notification()
+            send_discord_notification()
             return redirect('/')
         except:
             "Err.. trouble adding the activity to the plan"
@@ -106,11 +85,16 @@ def delete(id):
 
 
 def send_discord_notification():
-    webhook_url = ''
+    webhook_url = TOKEN
     data = {
-        "content": f"@everyone 📅 New Plan Created!"
+        "content": f"📆Test!!"
     }
-    requests.post(webhook_url, json=data)
+    response = requests.post(webhook_url, json=data)
+
+    if response.status_code == 204:
+        print("Notification sent!")
+    else:
+        print(f"Failed to send: {response.status_code}, {response.text}")
 
 
 if __name__ == '__main__':
